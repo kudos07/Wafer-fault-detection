5e08f5ba480048348941c80b4c83df3e82e96dd8
kudos07 <saransh9342@gmail.com>
2023-12-26 19:53:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+conda create -p env python=3.11.5 -y
+
+conda activate ./file_path
+
+
+pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Saransh",
+    author_email="saransh9342@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
